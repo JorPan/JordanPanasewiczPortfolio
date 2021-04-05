cb767da82ec516510fa807905a1cdb262d18924c
JorPan <jbpanasewicz@gmail.com>
2021-04-05 17:42:27 -0600

    contact page and header

---
+++ src/home/Resume.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Resume() {
+  return (
+    <div className="resume-screen">
+      <img
+        className="resume"
+        src="https://i.imgur.com/BqRNFLJ.png"
+        alt="JPResume"
+      />
+    </div>
+  );
+}
