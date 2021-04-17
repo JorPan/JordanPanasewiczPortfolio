b4b29c5185154e96b4d653f11ba9c6c1c8328ef1
JorPan <jbpanasewicz@gmail.com>
2021-04-16 18:35:09 -0600

    spacing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="footer">
+      <div className="space"></div>
       <p className="footer-tag">© JorPanTech 2021</p>
     </div>
   );
