6c10516d0a583ad63ebc59888742d2e99af251ca
JorPan <jbpanasewicz@gmail.com>
2021-06-03 14:24:59 -0600

    page ideas

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,5 +1,18 @@
 import React from "react";
+import DownloadLink from "react-download-link";
 
 export default function Home() {
-  return <div className="home-screen"></div>;
+  return (
+    <div className="home-screen">
+      <div className="home-top-section">
+        <div>
+          <img
+            className="circle-photo"
+            src="https://i.imgur.com/j289lch.jpg"
+            alt="jorpantech"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/home/Media.js
+++ src/home/Media.js
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function Media() {
-  return <div className="media-screen"></div>;
+  return (
+    <div className="media-screen">
+      <ul>
+        <li>Music</li>
+        <li>Visuals</li>
+        <li>Travel</li>
+      </ul>
+    </div>
+  );
 }
