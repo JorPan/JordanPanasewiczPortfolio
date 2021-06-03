6866be89445ba7dafd0b1f0df3264ed289bc6873
JorPan <jbpanasewicz@gmail.com>
2021-06-03 14:26:32 -0600

    projects list

--- src/home/Projects.js
+++ src/home/Projects.js
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function Projects() {
-  return <div className="projects-screen"></div>;
+  return (
+    <div className="projects-screen">
+      <ul>
+        <li>Mode-ify</li>
+        <li>Lyricize</li>
+        <li>SetLy</li>
+        <li>Herbals</li>
+        <li>Asteroids</li>
+        <li>DrumKit</li>
+        <li>SoundBoard</li>
+      </ul>
+    </div>
+  );
 }
