cf5d43645c110c7028a0eaa204520bfc63d4531d
JorPan <jbpanasewicz@gmail.com>
2021-07-07 15:05:11 -0600

    started project component

---
+++ src/components/Project.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Project({
+  name,
+  description,
+  techStack,
+  frontEndLink,
+  backEndLink,
+  demoLink,
+}) {
+  return <div className="project-card"></div>;
+}
--- src/home/Projects.js
+++ src/home/Projects.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Project from "../components/Project";
 
 export default function Projects() {
   return (
     <div className="projects-page">
       <ul className="projects-list">
+        <Project
+          name="Modify"
+          description="Chord and chord progression builder"
+          techStack="React, Node, Tonal, Sharp11"
+          frontEndLink="https://github.com/JorPan/musicly-frontend"
+          backEndLink="https://github.com/JorPan/musicly-backend"
+          demoLink="https://www.youtube.com/watch?v=HNiOuJO_xKI"
+        />
         <li>Mode-ify</li>
         <li>Lyricize</li>
         <li>SetLy</li>
