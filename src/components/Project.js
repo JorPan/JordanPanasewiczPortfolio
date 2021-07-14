5383ff83df225c7b951787e11db1f6ebb19d1936
JorPan <jbpanasewicz@gmail.com>
2021-07-14 17:13:35 -0600

    repo links

--- src/components/Project.js
+++ src/components/Project.js
@@ -14,6 +14,16 @@ export default function Project({
       <h1>{name}</h1>
       <h3>{description}</h3>
       <h5>{techStack}</h5>
+      <div className="repo-links">
+        <a className="repo link" href={frontEndLink} alt="front end repo">
+          Front End Repo
+        </a>
+        {backEndLink ? (
+          <a className="repo link" href={backEndLink} alt="back end repo">
+            Back End Repo
+          </a>
+        ) : null}
+      </div>
     </section>
   );
 }
