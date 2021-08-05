ae4163f0753897b35dde5e2822983321b17686ab
JorPan <jbpanasewicz@gmail.com>
2021-08-04 19:57:42 -0600

    bio

--- src/home/Home.js
+++ src/home/Home.js
@@ -16,28 +16,41 @@ export default function Home() {
           <h1>Hello!</h1>
           <p>...</p>
           <h2>
-            My Name is Jordan Panasewicz, and I am a passionate and eager to
-            learn Full Stack developer with a focus on User Experience and Work
-            Flow.
+            I've always been deeply obsessed with technology, from playing and
+            ‘designing’ video games as a child, to producing music
+            electronically, to photography & video editing, projection mapping,
+            electric vehicles, and more...
           </h2>
           <p>...</p>
           <h3>
-            I am passionate about ongoing education, and have chosen a path that
-            I believe will allow me to truly be a life long learner
+            My obsession with tech and my college experience in Marketing and
+            Tourism & Hospitality Management eventually pushed me toward a
+            career in software sales, where I worked for 6 years and learned
+            invaluable lessons, mostly around communication, problem solving,
+            and effort & time management.
           </h3>
           <p>...</p>
           <h4>
-            My obsession with technology is proven through my hobbies: Music
-            Production, Photography and Videography (and editing), Projection
-            Mapping, Electric Vehicles, Woodworking, Snowboarding, Skiing, Golf,
-            and more...
+            My eagerness and passion to continue learning, and my desire to be
+            more creative and utilize my knowledge and experience gained through
+            utilizing literally hundreds of softwares both at work and at home,
+            had me itching for something new. I decided to take some online
+            software engineering classes, and ultimately to go to Flatiron
+            School for Software Engineering, where I learned a ton about the
+            fundamentals, and deep dove into js, react, node, ruby, and rails -
+            as well as a bunch of 3rd party api’s and node packages that allowed
+            me to get extra creative.
           </h4>
           <p>...</p>
           <h4>
-            ..all of these things have allowed me to explore both software and
-            hardware deeply, assessing and analyzing user interfaces and
-            productivity workflows.
+            I now absolutely love both sides of the equation; being able to
+            speak the language and contribute to code bases, imagine and create
+            beautiful and powerful user interfaces, build and analyze databases,
+            and problem solve quickly and efficiently while communicating in a
+            more clear and concise way.
           </h4>
+          <p>...</p>
+          <h5>Thank you so much, I look forward to connecting soon!</h5>
         </div>
       </div>
     </div>
